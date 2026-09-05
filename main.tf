8c44fbe0d7269e285a0bedb9824c45f83dd95f3f
sdnexussolutions-ship-it <sdnexussolutions@gmail.com>
2026-09-04 21:52:54 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-c1c05bf8-90d9-4f7f-a17"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "sdnexussolutions-ship-it-example-bucket"
+  location = "US"
+}
